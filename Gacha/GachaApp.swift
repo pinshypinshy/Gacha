f0a464598a5b97d093cedc2a3bc83eaf21f13fa1
pinshypinshy <hrisyklit@gmail.com>
2026-02-09 18:03:13 +0900

    Initial Commit

---
+++ Gacha/GachaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GachaApp.swift
+//  Gacha
+//
+//  Created by 平石悠生 on 2026/02/09.
+//
+
+import SwiftUI
+
+@main
+struct GachaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
